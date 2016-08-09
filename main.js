d63d1fa06f6ed657b83e28314689963492d0e3d3
mick <mick@newmediafront.com>
2016-08-09 09:45:14 -0700

    added text file output

--- main.js
+++ main.js
@@ -11,16 +11,23 @@ fs.readdirSync('./png').forEach(function(file) {
     Jimp.read("./png/"+img+'.png').then(function (image) {
       image.dither565();
       var buf = new Buffer((image.bitmap.width * image.bitmap.height)*2);
+      var str = "";
       for (var x=0;x<image.bitmap.width;x++) {
     	  for (var y=0;y<image.bitmap.height;y++) {
           var b = image.getPixelColor(x,y);
           var rgba = Jimp.intToRGBA(b);
+
           var _p = rgba.r << 8 | rgba.g << 3 | rgba.b >> 3;
+
+          if (x>0 || y>0) str += ",";
+          str += _p;
+
           buf.writeUInt16LE(_p, c, 2);
           c+=2;
         }
       }
       fs.writeFile('./raw/'+img+'.dat', buf);
+      fs.writeFile('./str/'+img+'.txt', str);
     }).catch(function (err) {
       console.error(err);
     });
